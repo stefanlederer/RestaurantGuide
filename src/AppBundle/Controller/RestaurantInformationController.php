da121293bcb4838ddd1ca3b8c63b0d200f268cac
StefanLederer <stefan.lederer18@gmail.com>
2016-02-22 08:19:42 +0100

    [BE] submit comment in restaurant.html.twig

--- src/AppBundle/Controller/RestaurantInformationController.php
+++ src/AppBundle/Controller/RestaurantInformationController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Bewertung;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
@@ -15,7 +16,7 @@ class RestaurantInformationController extends Controller
         //get information, id's, ... of the restaurtant
         $repository = $this->getDoctrine()
             ->getRepository('AppBundle:Places');
-        $features = $repository->findOneBy( array('id' => $_GET["id"]));
+        $features = $repository->findOneBy(array('id' => $_GET["id"]));
 
         //get comment of restaurant
         $em = $this->getDoctrine()->getManager();
@@ -25,5 +26,37 @@ class RestaurantInformationController extends Controller
             'features' => $features,
             'comments' => $comments
         ));
+    }
+
+    /**
+     * @Route("/food", name="addedRating")
+     */
+    public function addedRating()
+    {
+        $userId = $this->get('security.token_storage')->getToken()->getUserId();
+        $placesId = $_GET["userId"];
+        $time = new \DateTime();
+        $time->format('H:i:s \O\n Y-m-d');
+
+        $request = Request::createFromGlobals();
+        $comment = $request->request->get("myComment");
+//        $rating = $request->request->get("rating");
+
+        if (strlen($comment) > 0) {
+            $rating = new Bewertung();
+
+            $rating->setUserId($userId);
+            $rating->setPlacesId($placesId);
+            $rating->setKommentar($comment);
+//            $rating->setBewertungen($rating);
+            $rating->setDate($time);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($rating);
+            $em->flush();
+        }
+        else {
+            print "error-comment!?";
+        }
     }
 }
