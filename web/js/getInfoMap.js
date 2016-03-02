f79dd97c87d93cce0b784348171a5097543c034a
StefanLederer <stefan.lederer18@gmail.com>
2016-03-02 22:28:13 +0100

    [FE] updated newRestaurant.html.twig get address information of marker and value them in input

--- web/js/getInfoMap.js
+++ web/js/getInfoMap.js
@@ -3,7 +3,12 @@
  */
 $(document).ready(function () {
 
-    console.log("gmaps");
+    var lng;
+    var lat;
+    var streetnumber;
+    var streetname;
+    var plz;
+    var place;
     //get Google Maps
     var map2 = new GMaps({
         div: '#newRestaurantMap',
@@ -17,6 +22,28 @@ $(document).ready(function () {
                 lng: oldlng = e.latLng.lng(),
                 title: 'Ausgewählter Ort'
             });
+            lat = e.latLng.lat();
+            lng = e.latLng.lng();
+            GMaps.geocode({
+                lng: lng,
+                lat: lat,
+                callback: function (results) {
+                    var allResult = results[0];
+                    console.log(allResult);
+                    streetnumber = allResult.address_components[0].long_name;
+                    streetname = allResult.address_components[1].long_name;
+                    plz = allResult.address_components[6].long_name;
+                    place = allResult.address_components[2].long_name;
+                }
+            });
+
+            $(".form-lat").val(lat);
+            $(".form-lng").val(lng);
+            $(".form-street").val(streetname);
+            $(".form-streetnumber").val(streetnumber);
+            $(".form-plz").val(plz);
+            $(".form-place").val(place);
+
             $("#newRestaurantForm").slideDown();
             $("#slider").removeClass("fa fa-sort-desc").addClass("fa fa-sort-asc");
         }
---
+++ web/js/search.js
@@ -0,0 +1,7 @@
+/**
+ * Created by stefan on 02.03.16.
+ */
+$(document).ready(function () {
+    var input = $('.search').val();
+    console.log(input);
+});
