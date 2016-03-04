5932bed0b000ce6c87e035879418d38d1f092640
StefanLederer <stefan.lederer18@gmail.com>
2016-03-04 19:29:34 +0100

    [BE] finished upload new Restaurant

--- src/AppBundle/Controller/NewRestaurantController.php
+++ src/AppBundle/Controller/NewRestaurantController.php
@@ -22,18 +22,18 @@ class NewRestaurantController extends Controller
         $restaurantPLZ = $request->request->get('restaurantplz');
         $restaurantplace = $request->request->get('restaurantplace');
         $file = $request->files->get('restaurantImage'); //file
-        $restaurantImg = $file->getClientOriginalName(); //Filename
+
         $north = $request->request->get('restaurantlng');
         $east = $request->request->get('restaurantlat');
 
         //get and proof image upload
         $targetDir = $this->get('kernel')->getRootDir() . '/../web/images';
 
-        print_r($restaurantImg);
 
         if (strlen($restaurantName) > 0 && strlen($restaurantStreet) > 0 && strlen($restaurantStreetNumber) > 0
             && strlen($restaurantPLZ) > 0 && strlen($restaurantplace) > 0 && strlen($north) > 0 && strlen($east) > 0
         ) {
+            $restaurantImg = $file->getClientOriginalName(); //Filename
 
             $file->move($targetDir, $restaurantImg);
 
